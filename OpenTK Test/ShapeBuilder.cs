280aa0c7a3cdc88ee30784fada5ba0e3f1bce155
Kiechant <kieren.ch@hotmail.com>
2016-11-10 23:49:10 +1030

    Imported ShapeBuilder and Triangulation classes

---
+++ OpenTK Test/ShapeBuilder.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/* Tool to create a shape defined as a series of vertices
+ joined by straight lines. Shapes can be built as meshes. */
+public class ShapeBuilder {
+
+	private List<Vector2> vertices = new List<Vector2>();
+	bool closed;
+
+	/* Prepares shape by creating first point.
+	 Does nothing if vertices have already been defined. */
+	public void Open(Vector2 point)
+	{
+		if (vertices.Count == 0)
+		{
+			vertices.Add(point);
+		}
+	}
+
+	/* Defines a straight line from the last added point to the passed point.
+	 Adds the passed point to the list of vertices. */
+	public void AddLine(Vector2 toPoint)
+	{
+		if (vertices.Count != 0)
+		{
+			vertices.Add(toPoint);
+		}
+	}
+
+	/* Defines an arc starting from the last added point sweeping around
+	 the z-axis at centre with the defined angular distance.
+	 Adds a number of points at regular intevals along the arc defined by steps.
+	 Steps must be at least 2. */
+	public void AddArc(Vector2 centre, float angularDistance, uint steps)
+	{
+		if (vertices.Count != 0 && steps >= 2)
+		{
+			Vector2 start = vertices.Last() - centre;
+			float delta = angularDistance / steps;
+			float angle = 0.0f;
+
+			for (int i = 1; Mathf.Abs(angle) < Mathf.Abs(angularDistance) - Vector2.kEpsilon; i++)
+			{
+				angle = delta * i;
+				Vector2 current = Rotate(start, angle);
+				vertices.Add(current + centre);
+			}
+		}
+	}
+
+	/* Sets the shape to be closed if there are at least three points.
+	 The shape will no longer be mutable and can be built as a mesh. */
+	public void Close()
+	{
+		if (vertices.Count >= 3)
+		{
+			if (vertices.Last() == vertices.First())
+			{
+				vertices.RemoveAt(vertices.Count - 1);
+			}
+			closed = true;
+		}
+	}
+
+	/* Returns a 2D vector rotated around the z-axis at the origin by angle. */
+	private static Vector2 Rotate(Vector3 point, float angle)
+	{
+		float cos = Mathf.Cos(angle);
+		float sin = Mathf.Sin(angle);
+
+		float x = point.x * cos - point.y * sin;
+		float y = point.x * sin + point.y * cos;
+
+		return new Vector2(x, y);
+	}
+
+	/* Transforms closed shape into a mesh and returns the result.
+	 Triangles of mesh are calculated using the Triangulation methods. */
+	public Mesh BuildMesh()
+	{
+		Mesh mesh = new Mesh();
+
+		if (closed)
+		{
+			List<int> triangles;
+			Triangulation.Process(vertices, out triangles);
+
+			int n = vertices.Count;
+			Vector3[] newVertices = new Vector3[n];
+			for (int i = 0; i < vertices.Count; i++)
+			{
+				Vector2 vertex = vertices[i];
+				newVertices[i] = new Vector3(vertex.x, vertex.y, 0);
+			}
+			int[] newTriangles = triangles.ToArray();
+
+			mesh.vertices = newVertices;
+			mesh.triangles = newTriangles;
+		}
+
+		return mesh;
+	}
+
+	public Vector3[] GetVertices()
+	{
+		Vector3[] newVertices = new Vector3[vertices.Count];
+		for (int i = 0; i < vertices.Count; i++)
+		{
+			Vector2 vertex = vertices[i];
+			newVertices[i] = new Vector3(vertex.x, vertex.y, 0);
+		}
+		return newVertices;
+	}
+
+	public void Print()
+	{
+		foreach(Vector2 vertex in vertices)
+		{
+			Debug.Log(vertex);
+		}
+	}
+}
---
+++ OpenTK Test/Triangulation.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Triangulation
+{
+	const float EPSILON = 0.00001f;
+	
+	/* Triangulate a polygon defined by sequential vertices.
+	 Sets triangles as sets of three consecutive vertex indices.
+	 Returns true if successful. */
+	public static bool Process(List<Vector2> vertices, out List<int> triangles)
+	{
+		// Allocate and initialise vertices in polygon
+
+		triangles = new List<int>();
+		if (vertices.Count < 3) return false;
+
+		int[] V = new int[vertices.Count];
+
+		// Polygon in V should be counter-clockwise
+
+		if (Area(vertices) > 0.0f)
+		{
+			for (int i = 0; i < vertices.Count; i++)
+			{
+				V[i] = i;
+			}
+		}
+
+		else
+		{
+			for (int i = 0; i < vertices.Count; i++)
+			{
+				V[i] = (vertices.Count - 1) - i;
+			}
+		}
+
+		// Remove n - 2 vertices, creating 1 triangle each time
+		// Detects errors and returns false if found
+
+		// Current number of vertices
+		int n = vertices.Count;
+		// Tracks current vertex
+		int v = n - 1;
+		// Error detection variable
+		int count = 2 * n;
+
+		while (n > 2)
+		{
+			// Indicates a loop and a probable bad polygon
+			if (--count <= 0) return false;
+
+			// Finds three consecutive vertices in polygon (u, v, w)
+			int u = v;		if (u >= n) u = 0;
+			v = u + 1;		if (v >= n) v = 0;
+			int w = v + 1;	if (w >= n) w = 0;
+
+			if (Snip(vertices, u, v, w, n, V))
+			{
+				// Defines triangle
+				triangles.Add(V[u]);
+				triangles.Add(V[v]);
+				triangles.Add(V[w]);
+
+				// Remove v from remaining polygon
+
+				int s = v;
+				int t = v + 1;
+
+				while (t < n)
+				{
+					V[s] = V[t];
+					s++; t++;
+				}
+
+				n--;
+
+				// Reset error detection counter
+				count = 2 * n;
+			}
+		}
+
+		return true;
+	}
+
+	/* Calculates area of polygon defined by vertices and returns value. */
+	public static float Area(List<Vector2> vertices)
+	{
+		int n = vertices.Count;
+		float area = 0.0f;
+
+		for (int p = n - 1, q = 0; q < n; p = q++)
+		{
+			Vector2 vp = vertices[p], vq = vertices[q];
+			area += vp.x * vq.y - vq.x * vp.y;
+		}
+
+		return area * 0.5f;
+	}
+
+	/* Returns true if point P lies within points A, B and C. */
+	public static bool InsideTriangle(Vector2 A, Vector2 B, Vector2 C, Vector2 P)
+	{
+		// Vectors outline triangle ABC
+		Vector2 va = C - B;
+		Vector2 vb = A - C;
+		Vector2 vc = B - A;
+
+		// Vectors from points A, B and C to point P
+		Vector2 pa = P - A;
+		Vector2 pb = P - B;
+		Vector2 pc = P - C;
+
+		// Cross products of vector components emerging from the same point
+		float ca = va.x * pb.y - va.y * pb.x;
+		float cb = vb.x * pc.y - vb.y * pc.x;
+		float cc = vc.x * pa.y - vc.y * pa.x;
+
+		return ((ca >= 0.0f) && (cb >= 0.0f) && (cc >= 0.0f));
+	}
+
+	/* Returns true if vertices u, v, w should be snipped from the polygon
+	 to form a new triangle. Vertex n is the current vertex being processed. */
+	private static bool Snip(List<Vector2> vertices, int u, int v, int w, int n, int[] V)
+	{
+		Vector2 A, B, C;
+		A = vertices[V[u]];
+		B = vertices[V[v]];
+		C = vertices[V[w]];
+
+		if ((((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))) < EPSILON) return false;
+
+		for (int p = 0; p < n; p++)
+		{
+			if (p == u || p == v || p == w) continue;
+			Vector2 P = vertices[V[p]];
+			if (InsideTriangle(A, B, C, P)) return false;
+		}
+
+		return true;
+	}
+}
+
